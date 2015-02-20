d21c243720e4cf28b839d7cdd312ef269e8c401a
Andres Roldan <afroldann@gmail.com>
2015-02-20 09:55:39 -0500

    wo config ignore

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'manaWBD');
+define('DB_NAME', 'manawynwood');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
